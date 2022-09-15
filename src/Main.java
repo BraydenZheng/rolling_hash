d6b98c13e8470e8b1e837566813e5337661c3f80
Brayden Zheng <imfczbf@gmail.com>
2022-09-14 22:18:21 -0600

    feat: init project

---
+++ src/Main.java
@@ -0,0 +1,139 @@
+import java.util.HashMap;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String s1 = "GAACGCGGCACACTTGATGAAGTCAAGACGAAATTAGACGTGCGGAAGACCGTCAAAATTATCGTGCAGAAGA";
+		String s2 = "TATGCCCGCGATAGAACATGAAAATATCATTGACGTGGAATACAAAGGTAGAAAGGCGGTCATAGAAGCAAAATCCGATA";
+//		String s1 = "GAACCGCCG";
+//		String s2 = "TAATGCCC";
+
+		final int b = 17;
+		final int p = 31513;
+		HashMap<String, Integer> res = commonSubstring(s1, s2, 7, b, p);
+		System.out.println(res);
+	}
+
+	private static HashMap<String, Integer> commonSubstring(String s1, String s2, int k, int b, int p) {
+		HashMap<String, Integer> res = new HashMap<>();
+		HashMap<Double, String> hashTable = new HashMap<>();
+		String ori_first = s1.substring(0, k);
+
+		// Fill hashTable with subString hash value
+		double beginHash = multiPlicativeHash(s1, b, k, 0, p);
+		hashTable.put(beginHash, ori_first);
+
+		double previousHash = beginHash;
+		for (int i = 1; i < s1.length() - k; i++) {
+			String ori = s1.substring(i, i + k);
+			double hash = rollingHash(s1, b, k, i, p, previousHash);
+
+			// debug
+			double realHash = multiPlicativeHash(s1, b, k, i, p);
+
+			hashTable.put(hash, ori);
+			previousHash = hash;
+		}
+
+		String sec_first = s2.substring(0, k);
+		// Search second String from hashTable
+		double beginHash_2 = multiPlicativeHash(s2, b, k, 0, p);
+		String match = hashTable.get(beginHash_2);
+		if (match != null)
+		{
+			if (res.get(match) == null)
+			{
+				res.put(match, 0);
+			}
+			else
+			{
+				res.put(match, res.get(match) + 1);
+			} ;
+		}
+
+		previousHash = beginHash_2;
+		for (int i = 1; i <= s2.length() - k; i++) {
+//			String sec = s2.substring(i, i + k);
+			double hash = rollingHash(s2, b, k, i, p, previousHash);
+
+			match = hashTable.get(hash);
+			if (match != null)
+			{
+				if (res.get(match) == null)
+				{
+					res.put(match, 1);
+				}
+				else
+				{
+					res.put(match, res.get(match) + 1);
+				} ;
+			}
+			previousHash = hash;
+		}
+		return res;
+	}
+
+	/**
+	 * use for first time count hash (without referring other items)
+	 * @param x input string
+	 * @param b random salt
+	 * @param k given range
+	 * @param i start index
+	 * @param p prime number
+	 * @return hash value
+	 */
+//	private static double multiPlicativeHash(String x, int b, int k, int i, int p) {
+//		double res = 0;
+//		for (int j = 0; j < k; j++) {
+//			//first index item to start with
+//			char curr = x.charAt(i + j);
+//			res = (res + (int) curr * Math.pow(b, k - i - 1)) % p;
+//		}
+//		return res;
+//	}
+
+
+	/**
+	 * use for first time count hash (without referring other items)
+	 * @param x input string
+	 * @param b random salt
+	 * @param k given range
+	 * @param i start index
+	 * @param p prime number
+	 * @return hash value
+	 */
+	private static double multiPlicativeHash(String x, int b, int k, int i, int p) {
+		double sum = 0;
+		for (int j = 0; j < k; j++) {
+			//first index item to start with
+			char curr = x.charAt(i + j);
+			sum = sum + (int) curr * Math.pow(b, k - j - 1);
+		}
+		return sum % p;
+	}
+
+	/**
+	 * Rolling hash function for 1 unit at each time
+	 * @param x
+	 * @param b
+	 * @param k
+	 * @param i
+	 * @param p
+	 * @param previousHash
+	 * @return
+	 */
+	private static double rollingHash(String x, int b, int k, int i, int p, double previousHash) {
+		double res = previousHash;
+		char first = x.charAt(i - 1);
+		char insert = x.charAt(i + k - 1);
+		res = ((previousHash - (int) first * Math.pow(b, k - 1) + p) % p) * b;
+		res = (res % p + insert) % p;
+		return res;
+	}
+
+private static int stringToInt(String s) { int total = 0; for (int i = 0; i < s.length(); i++) { total += s.charAt(i);
+		}
+		return total;
+	}
+}
